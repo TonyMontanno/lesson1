c44d7875316ca6f88c53a0b5c9eaf47ad9f420f2
Anton <Crazy_antonio@mail.ru>
2023-09-05 20:48:33 +0300

    ffd

---
+++ src/main/java/calculator/Main.java
@@ -0,0 +1,56 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+
+
+    }
+
+
+    public static String calc(String input) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+        scanner.close();
+
+        String[] arrayInput = input.split(" ");
+        int dlina = arrayInput.length;
+        if (dlina != 3) {
+            throw new Exception("throws Exception //т.к. формат математической операции не " +
+                    "удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+        int firstOperand = Integer.parseInt(arrayInput[0]);
+        String mathOperator = arrayInput[1];
+        int secondOperand = Integer.parseInt(arrayInput[2]);
+        int otvet = 0;
+
+        if (firstOperand > 10 || secondOperand > 10 || firstOperand < 1 || secondOperand < 1) {
+            throw new Exception("Числа операндов не входят в нужный диапазон");
+        }
+
+        switch (mathOperator) {
+            case "+":
+                otvet = firstOperand + secondOperand;
+                break;
+            case "-":
+                otvet = firstOperand - secondOperand;
+                break;
+            case "*":
+                otvet = firstOperand * secondOperand;
+                break;
+            case "/":
+                otvet = firstOperand / secondOperand;
+                break;
+            default:
+                throw new Exception("Недопустимая операция");
+        }
+        System.out.println(otvet);
+
+
+        return String.valueOf(otvet);
+    }
+}
+
